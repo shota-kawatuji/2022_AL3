50ec742c60cfbd7ff448314e9b0b068038e55c53
ShotaKawatuji <k021g1403@g.neec.ac.jp>
2022-04-15 13:25:05 +0900

    ２-１取り組み前

--- main.cpp
+++ main.cpp
@@ -18,7 +18,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
 	// ゲームウィンドウの作成
 	win = WinApp::GetInstance();
-	win->CreateGameWindow();
+	win->CreateGameWindow("LE2C_11_カワツジ_ショウタ_AL3");
 
 	// DirectX初期化処理
 	dxCommon = DirectXCommon::GetInstance();
--- scene/GameScene.cpp
+++ scene/GameScene.cpp
@@ -16,6 +16,7 @@ void GameScene::Initialize() {
 	debugText_ = DebugText::GetInstance();
 }
 
+
 void GameScene::Update() {}
 
 void GameScene::Draw() {
